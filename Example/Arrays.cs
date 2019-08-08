d779f3f2d9395713ead9661eea18e9439fd361d8
Tyler Mckee <tmmckee97@gmail.com>
2019-08-08 15:36:52 -0400

    CHANGED

---
+++ Example/Arrays.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Arrays
+    {
+        public int[] ArrayA = new int[] {0, 2, 4, 6, 8, 10};
+        public int[] ArrayB = new int[] {1, 3, 5, 7, 2};
+        public int[] ArrayC = new int[] {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9};
+
+        public int[] MakeArray()
+        {
+            
+            int input = 0;
+            Console.WriteLine("Enter a number for how many integers are in your array.");
+            input = int.Parse(Console.ReadLine());
+            int[] cats = new int[input];
+
+            Console.WriteLine("Fill in your desired integers.");
+            for (int i = 0; i < cats.Length; i++)
+            {
+                cats[i] = int.Parse(Console.ReadLine());
+            }
+            return cats;
+        }
+
+        public double Sum(int[] array)
+        {
+            Console.WriteLine("Total sum of elements.");
+            double total = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                total += array[i];
+            }
+            Console.WriteLine(total);
+            return total;
+        }
+
+        public void Average(double counter, double sum)
+        {
+            double avg = sum / counter;
+            Console.WriteLine($"Average is {avg}");
+        }
+
+        public void Reverse(int[] array)
+        {
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public int[] Rotate(Direction direction, int places, int[] array) 
+        {
+            if (direction == Direction.Left)
+            {
+                
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = array[(i + places)%array.Length];
+                }
+
+                return array;
+            }
+            else
+            {
+                for (int i = 0; i >= array.Length; i++)
+                {
+                    array[i] = array[(i - places)%array.Length];
+                }
+
+                return array;
+            }
+        }
+
+        public void Display(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public int[] Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                if (array[i] > array[i+1])
+                {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    i = 0;
+                }
+            }
+
+            return array;
+        }
+
+        public int[] SortDesc(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] < array[i + 1])
+                {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    i = 0;
+                }
+
+            }
+
+            return array;
+        }
+
+        public int[] Combine(int[] firstArray, int[] secondArray)
+        {
+            int[] combo = new int[firstArray.Length+secondArray.Length];
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                combo[i] = firstArray[i];
+            }
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                combo[i + firstArray.Length] = secondArray[i];
+            }
+            return combo;
+        }
+
+        public enum Direction
+        {
+            Left = 0,
+            Right =1
+        }
+
+        public int Duplicates(int[] array)
+        {
+            int totalNumDuplicates = 0;
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                if (array[i]==array[i+1])
+                {
+                    int temp = array[i];
+                    totalNumDuplicates += 1;
+                    array[i + 1] = temp;
+                }
+                
+            }
+            return totalNumDuplicates;
+        }
+
+        public void UniqueElements(int[] array)
+        {
+            int unique = 0;
+            int[] tempCopy = new int[array.Length];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    unique += 1;
+                }
+
+                if (unique <= 1)
+                {
+                    Console.WriteLine($"{array[i]} is unique.");
+                }
+            }
+            
+        }
+
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,88 @@ namespace Example
     {
         static void Main(string[] args)
         {
+            Arrays arrays = new Arrays();
+
+            //int[] array = arrays.MakeArray();
+            //int[] array1 = arrays.MakeArray();
+
+            //int[] combinedArray = arrays.Combine(array, array1);
+            //arrays.SortDesc(combinedArray);
+            //Console.WriteLine("Combined and assorted array: ");
+            //arrays.Display(combinedArray);
+
+            //double sum = arrays.Sum(array);
+            //arrays.Average(array.Length, sum);
+
+            //arrays.Reverse(array);
+
+            //int[] arrayA = arrays.Rotate(Arrays.Direction.Left, 2, arrays.ArrayA);
+            //int[] arrayB = arrays.Rotate(Arrays.Direction.Right, 2, arrays.ArrayB);
+            //int[] arrayC = arrays.Rotate(Arrays.Direction.Left, 4, arrays.ArrayC);
+
+            //Counting, Summing, computing mean
+            int[] arrayA = arrays.ArrayA;
+            int[] arrayB = arrays.ArrayB;
+            int[] arrayC = arrays.ArrayC;
+            double sumA = arrays.Sum(arrayA);
+            double sumB = arrays.Sum(arrayB);
+            double sumC = arrays.Sum(arrayC);
+            Console.WriteLine("");
+            arrays.Average(arrayA.Length, sumA);
+            arrays.Average(arrayB.Length, sumB);
+            arrays.Average(arrayC.Length, sumC);
+
+            Console.WriteLine("");
+
+            //Reversing arrays
+            Console.WriteLine("ArrayA reversed:");
+            arrays.Reverse(arrayA);
+            Console.WriteLine("ArrayB reversed:");
+            arrays.Reverse(arrayB);
+            Console.WriteLine("ArrayC reversed:");
+            arrays.Reverse(arrayC);
+
+            int[] rotatedArrayA=arrays.Rotate(Arrays.Direction.Left, 2, arrayA);
+            int[] rotatedArrayB = arrays.Rotate(Arrays.Direction.Right, 2, arrayB);
+            int[] rotatedArrayC = arrays.Rotate(Arrays.Direction.Left, 4, arrayC);
+            Console.WriteLine("Rotated arrayA left by two places: ");
+            arrays.Display(rotatedArrayA);
+            Console.WriteLine("Rotated arrayB right by two places: ");
+            arrays.Display(rotatedArrayB);
+            Console.WriteLine("Rotated arrayC left by four places");
+            arrays.Display(rotatedArrayC);
+
+            Console.WriteLine("CHANGED");
+
+
+            //int[] arrangedArray = arrays.Sort(arrays.ArrayC);
+            //arrays.Display(arrangedArray);
+            //int numberOfDuplicates = arrays.Duplicates(arrangedArray);
+            //Console.WriteLine($"Number of duplicates is {numberOfDuplicates}");
+
+            //arrays.UniqueElements(array);
+
+
+
+            //Deep copy of array
+            //int[] cats2 = new int[cats.Length];
+            //for (int i = 0; i < cats.Length; i++)
+            //{
+            //    cats2[i] = cats[i];
+            //}
+            //Console.WriteLine("First array printed:");
+            //for (int i = 0; i < cats.Length; i++)
+            //{
+            //    Console.WriteLine(cats[i]);
+            //}
+            //Console.WriteLine("Second array printed:");
+            //for (int i = 0; i < cats2.Length; i++)
+            //{
+            //    Console.WriteLine(cats2[i]);
+            //}
+
         }
+
+
     }
 }
